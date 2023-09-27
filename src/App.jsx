64afdb53f3a6a2afb1b0b45dafc53297a05573fa
kshitijofficial <srivastavakshitijprofessional@gmail.com>
2023-09-27 14:46:30 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
+import UserInfo from './context/UserInfo';
 import Dashboard from './components/Dashboard';
 import "./App.css"
-
-const user = {
-  name: 'John Doe',
-  email: 'john.doe@example.com',
-  age: 30,
-};
 
 const App = () => {
   return (
     <div className="app">
       <h1>User Dashboard</h1>
-      <Dashboard user={user} />
+      <UserInfo>
+        <Dashboard/>
+      </UserInfo>
     </div>
   );
 };
---
+++ src/context/UserInfo.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import UserContext from "./userContext";
+const UserInfo = ({children})=>{
+
+  const [user,setUser]=useState({
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    age: 30,
+  })
+
+  const changeUser =()=>{
+    setUser({
+      name: 'XYZ',
+      email: 'xyz@example.com',
+      age: 20,
+    })
+  }
+  
+
+      return(
+        <UserContext.Provider value={{user,changeUser}}>
+           {children}
+        </UserContext.Provider>
+      )
+      
+}
+export default UserInfo;
